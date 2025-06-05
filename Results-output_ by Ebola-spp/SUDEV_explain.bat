d4dfc060034c1b9c747d5be1164af7e9a87ac5c9
PAASI GEORGE <georgepaasi8@gmail.com>
2025-06-05 12:14:25 +0300

    Add files via upload

---
+++ Results-output_ by Ebola-spp/SUDEV_explain.bat
@@ -0,0 +1,2 @@
+java -mx2010m -cp "/C:/Users/gpaasi/Downloads/tutorial-data/maxent/maxent/maxent.jar;C:\Users\gpaasi\Downloads\tutorial-data\maxent\maxent\maxent.jar" density.Explain -l "C:\Users\gpaasi\Downloads\tutorial-data\output_spp new\SUDEV.lambdas" -c "C:\Users\gpaasi\Downloads\tutorial-data\output_spp new\SUDEV.asc" C:\Users\gpaasi\Downloads\tutorial-data\aligned
+@if errorlevel 1 pause
---
+++ Results-output_ by Ebola-spp/ZEV_explain.bat
@@ -0,0 +1,2 @@
+java -mx2010m -cp "/C:/Users/gpaasi/Downloads/tutorial-data/maxent/maxent/maxent.jar;C:\Users\gpaasi\Downloads\tutorial-data\maxent\maxent\maxent.jar" density.Explain -l "C:\Users\gpaasi\Downloads\tutorial-data\output_spp new\ZEV.lambdas" -c "C:\Users\gpaasi\Downloads\tutorial-data\output_spp new\ZEV.asc" C:\Users\gpaasi\Downloads\tutorial-data\aligned
+@if errorlevel 1 pause
