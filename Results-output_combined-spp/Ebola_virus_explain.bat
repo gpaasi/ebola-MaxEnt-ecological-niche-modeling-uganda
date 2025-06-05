56424e434085930a226e69991eb57fadb63ad0a9
PAASI GEORGE <georgepaasi8@gmail.com>
2025-06-05 12:16:35 +0300

    Add files via upload

---
+++ Results-output_combined-spp/Ebola_virus_explain.bat
@@ -0,0 +1,2 @@
+java -mx2010m -cp "/C:/Users/gpaasi/Downloads/tutorial-data/maxent/maxent/maxent.jar;C:\Users\gpaasi\Downloads\tutorial-data\maxent\maxent\maxent.jar" density.Explain -l C:\Users\gpaasi\Downloads\tutorial-data\output_all\Ebola_virus.lambdas -c C:\Users\gpaasi\Downloads\tutorial-data\output_all\Ebola_virus.asc C:\Users\gpaasi\Downloads\tutorial-data\aligned
+@if errorlevel 1 pause
